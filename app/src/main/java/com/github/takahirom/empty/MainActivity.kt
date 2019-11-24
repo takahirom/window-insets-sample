ff629a6c9fe6b8db2718b56d209371cef44a6f82
takahirom <takam.dev@gmail.com>
2019-11-24 18:13:40 +0900

    Implement RecyclerView sample

--- app/src/main/java/com/github/takahirom/empty/MainActivity.kt
+++ app/src/main/java/com/github/takahirom/empty/MainActivity.kt
@@ -2,24 +2,50 @@ package com.github.takahirom.empty
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.updatePadding
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val root = findViewById<View>(R.id.root)
-//        root.setOnApplyWindowInsetsListener { v, insets ->
-//            root.updatePadding(
-//                top = insets.systemWindowInsetTop,
-//                bottom = insets.systemWindowInsetBottom
-//            )
-//            return@setOnApplyWindowInsetsListener insets
-//        }
-        root.systemUiVisibility =
+        val recyclerView = findViewById<RecyclerView>(R.id.recycler)
+        recyclerView.setOnApplyWindowInsetsListener { v, insets ->
+            recyclerView.updatePadding(
+                top = insets.systemWindowInsetTop,
+                bottom = insets.systemWindowInsetBottom
+            )
+            return@setOnApplyWindowInsetsListener insets
+        }
+        recyclerView.systemUiVisibility =
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE or
                     View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+        recyclerView.adapter = object : RecyclerView.Adapter<RecyclerView.ViewHolder?>() {
+            override fun onCreateViewHolder(
+                parent: ViewGroup,
+                viewType: Int
+            ): RecyclerView.ViewHolder {
+                return object : RecyclerView.ViewHolder(
+                    TextView(parent.context).apply {
+                        setTextAppearance(
+                            this@MainActivity,
+                            R.style.TextAppearance_AppCompat_Headline
+                        )
+                    }
+                ) {
+                }
+            }
+
+            override fun getItemCount(): Int = 50
+
+            override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+                (holder.itemView as TextView).text = "item:" + position.toString()
+            }
+        }
+
     }
 }
